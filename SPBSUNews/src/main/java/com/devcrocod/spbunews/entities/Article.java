8ace28dea6b1b9879c11c836c23f6d7387cf4192
bagsw <vpburtsev@gmail.com>
2017-05-25 21:40:20 +0300

    javadoc

--- SPBSUNews/src/main/java/com/devcrocod/spbunews/entities/Article.java
+++ SPBSUNews/src/main/java/com/devcrocod/spbunews/entities/Article.java
@@ -4,7 +4,10 @@ import javax.persistence.*;
 import java.sql.Date;
 import java.util.Arrays;
 
-
+/**
+ *
+ * This class describes articles
+ */
 @Entity
 @Table(name ="article")
 public class Article {
@@ -17,10 +20,25 @@ public class Article {
     private User editorArticleId;
     private Comment commentArticleId;
 
+    /**
+     *
+     */
     public Article() {
 
     }
 
+    /**
+     * Initiates an article
+     * 
+     * @param idArticle
+     * @param title
+     * @param date
+     * @param content
+     * @param image
+     * @param valueSum
+     * @param editorArticleId
+     * @param commentArticleId
+     */
     public Article(int idArticle, String title, Date date, String content, byte[] image, Integer valueSum, User editorArticleId, Comment commentArticleId) {
         this.idArticle = idArticle;
         this.title = title;
@@ -32,82 +50,162 @@ public class Article {
         this.commentArticleId = commentArticleId;
     }
 
+    /**
+     * Gets existing article id
+     *
+     * @return article id
+     */
     @Id
     @Column(name = "id_article")
     public Integer getIdArticle() {
         return idArticle;
     }
 
+    /**
+     * Sets new article id
+     *
+     * @param idArticle
+     */
     public void setIdArticle(Integer idArticle) {
         this.idArticle = idArticle;
     }
 
+    /**
+     *  Gets title from article
+     * 
+     * @return title of article
+     */
     @Basic
     @Column(name = "title")
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Sets title for article
+     *
+     * @param name for title
+     */
     public void setTitle(String name) {
         this.title = name;
     }
 
+    /**
+     * Gets date from article
+     *
+     * @return date of creation
+     */
     @Basic
     @Column(name = "date")
     public Date getDate() {
         return date;
     }
 
+    /**
+     * Sets date for article
+     *
+     * @param date for aritcle
+     */
     public void setDate(Date date) {
         this.date = date;
     }
 
+    /**
+     * Gets content from article
+     *
+     * @return content of article
+     */
     @Basic
     @Column(name = "content")
     public String getContent() {
         return content;
     }
 
+    /**
+     * Sets content for article
+     * 
+     * @param content to be set
+     */
     public void setContent(String content) {
         this.content = content;
     }
 
+    /**
+     * Gets image from article
+     * 
+     * @return image
+     */
     @Basic
     @Column(name = "image")
     public byte[] getImage() {
         return image;
     }
 
+    /**
+     * Sets image for article
+     *
+     * @param image to be set
+     */
     public void setImage(byte[] image) {
         this.image = image;
     }
 
+    /**
+     *  Gets rating of article in valueSum
+     * 
+     * @return valueSum
+     */
     @Basic
     @Column(name = "value_sum")
     public Integer getValueSum() {
         return valueSum;
     }
 
+    /**
+     * Sets rating of article
+     *
+     * @param valueSum refers to rating of article
+     */
     public void setValueSum(Integer valueSum) {
         this.valueSum = valueSum;
     }
 
+    /**
+     * Gets editor's id from article
+     *
+     * @return edtorArticleId
+     */
     @ManyToOne
     @JoinColumn(name = "editor_article_id")
     public User getEditorArticleId() {
         return editorArticleId;
     }
 
+    /**
+     * Sets editor's id to article
+     * 
+     * @param editorArticleId
+     */
     public void setEditorArticleId(User editorArticleId) {
         this.editorArticleId = editorArticleId;
     }
 
+    /**
+     * Gets comment id from article
+     *
+     * @return commentArticleId
+     */
     @ManyToOne
     @JoinColumn(name = "comment_article_id")
     public Comment getCommentArticleId() {
         return commentArticleId;
     }
 
+    /**
+     * Sets comment id to article
+     * 
+     * @param commentArticleId
+     */
     public void setCommentArticleId(Comment commentArticleId) {
         this.commentArticleId = commentArticleId;
     }
