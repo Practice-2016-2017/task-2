557dae9f1dfd6abff899856b9d0f42a3c3244cca
bagsw <vpburtsev@gmail.com>
2017-05-25 23:54:45 +0300

    javadoc

--- SPBSUNews/src/main/java/com/devcrocod/spbunews/objects/SearchCriteria.java
+++ SPBSUNews/src/main/java/com/devcrocod/spbunews/objects/SearchCriteria.java
@@ -6,6 +6,10 @@ import org.springframework.stereotype.Component;
 
 import java.sql.Date;
 
+/**
+ * This class defines criteria of search
+ * 
+ */
 @Component
 @Scope("singleton")
 public class SearchCriteria {
@@ -16,26 +20,55 @@ public class SearchCriteria {
 
     private Date date;
 
+    /**
+     * Gets text
+     *
+     * @return text
+     */
     public String getText() {
         return text;
     }
 
+    /**
+     * Sets text of search criteria
+     *
+     * @param text to be set
+     */
     public void setText(String text){
         this.text = text;
     }
 
+    /**
+     * Gets search type
+     *
+     * @return searchType
+     */
     public SearchType getSearchType() {
         return searchType;
     }
 
+    /**
+     *
+     * @param searchType
+     */
     public void setSearchType(SearchType searchType) {
         this.searchType = searchType;
     }
 
+    /**
+     * Gets date
+     *
+     * @return date
+     */
     public Date getDate() {
         return date;
     }
 
+    /**
+     * Sets date as search criteria
+     * 
+     * @param date to be set
+     */
     public void setDate(Date date) {
         this.date = date;
     }
